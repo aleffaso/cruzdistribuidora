c949063e2a0e176b3a80a2bf465f319ce5ad2191
Aleff <aleff.a.s.o@gmail.com>
2022-04-10 23:46:32 -0300

    Updating webpage

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -103,7 +103,7 @@
       if (window.scrollY > 100) {
         backtotop.classList.add('active')
       } else {
-        backtotop.classList.remove('active')
+        backtotop.classList.add('active')
       }
     }
     window.addEventListener('load', toggleBacktotop)
@@ -157,6 +157,18 @@
       }
     }
   });
+
+    /**
+   * Hero carousel indicators
+   */
+     let heroCarouselIndicators = select("#hero-carousel-indicators")
+     let heroCarouselItems = select('#heroCarousel .carousel-item', true)
+   
+     heroCarouselItems.forEach((item, index) => {
+       (index === 0) ?
+       heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "' class='active'></li>":
+         heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "'></li>"
+     });
 
   /**
    * Preloader
