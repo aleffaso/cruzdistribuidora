e4ee6112044d31d97321861c497f815263c568e2
Aleff <aleff.a.s.o@gmail.com>
2022-04-13 16:34:29 -0300

    mkt propaganda

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -5,3 +5,14 @@ function deleteCheck(event, form){
         form.submit();
     }
 }
+
+//Filter list
+
+$(document).ready(function(){
+    $("#listSearch").on("keyup", function() {
+      var value = $(this).val().toLowerCase();
+      $("#myList #listItem").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+    });
+  });
